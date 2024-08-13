41f6a3cbe1476a4284ea8f649bd9ae7957b5b360
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-13 16:53:53 +0300

    Add example for fileData and small fixes

--- src/main/java/dkcorp/caselab_file_service/dto/file/FileUploadDto.java
+++ src/main/java/dkcorp/caselab_file_service/dto/file/FileUploadDto.java
@@ -12,13 +12,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class FileUploadDto {
-    @NotBlank(message = "File data is required")
+    @NotBlank(message = "File data is required. Example: VGhpcyBpcyB0aGUgZmlsZSBkZXNjcmlwdGlvbg==")
     private String fileData;
 
-    @NotBlank(message = "Title is required. Example: FILE_TITLE")
-    @Size(min = 1, max = 32, message = "Title should be between 1 and 32 characters. Example: FILE_TITLE")
+    @NotBlank(message = "Title is required. Example: File title.")
+    @Size(min = 1, max = 32, message = "Title should be between 1 and 32 characters. Example: File title.")
     private String title;
 
-    @Size(max = 500, message = "Description should not exceed 500 characters. Example: This is example of description.")
+    @Size(max = 500, message = "Description should not exceed 500 characters. Example: This is the file description.")
     private String description;
 }
