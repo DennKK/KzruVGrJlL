477ffb90e7c85f71f4f1980dcc3ea8374e8eef5d
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-13 14:18:55 +0300

    Add api version

--- src/main/java/dkcorp/caselab_test_task/controller/FileController.java
+++ src/main/java/dkcorp/caselab_test_task/controller/FileController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/files")
+@RequestMapping("/api/v1/files")
 @RequiredArgsConstructor
 @Tag(name = "File API", description = "Operations related to files")
 public class FileController {
