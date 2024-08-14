fc9f6216f5391aefd8de14491898321ad1c8ba54
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-14 14:00:02 +0300

    Create get files method with params

--- src/main/java/dkcorp/caselab_file_service/controller/FileController.java
+++ src/main/java/dkcorp/caselab_file_service/controller/FileController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,6 +24,17 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "File API", description = "Operations related to files")
 public class FileController {
     private final FileService fileService;
+
+    @GetMapping
+    @Operation(summary = "Get a paginated list of files", description = "Retrieves a paginated and sorted list of files.")
+    public ResponseEntity<Page<FileDto>> getFiles(
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "2") int size,
+            @RequestParam(defaultValue = "creationDate") String sortBy
+    ) {
+        Page<FileDto> files = fileService.findAllFiles(page, size, sortBy);
+        return new ResponseEntity<>(files, HttpStatus.OK);
+    }
 
     @PostMapping("/upload")
     @Operation(summary = "Upload a new file", description = "Creates a new file and returns its ID.")
