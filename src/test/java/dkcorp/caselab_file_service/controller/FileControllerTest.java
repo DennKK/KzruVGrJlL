e94156590c506b048c5a5efe95047afd730904ba
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-14 14:12:08 +0300

    Add test for getFiles method

--- src/test/java/dkcorp/caselab_file_service/controller/FileControllerTest.java
+++ src/test/java/dkcorp/caselab_file_service/controller/FileControllerTest.java
@@ -9,10 +9,14 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -30,6 +34,38 @@ class FileControllerTest {
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void getFiles_returnsPagedFileDtos() {
+        int page = 0;
+        int size = 2;
+        String sortBy = "creationDate";
+
+        List<FileDto> fileDtoList = Arrays.asList(
+                FileDto.builder()
+                        .fileData("VGhpcyBpcyB0aGUgZmlsZSBkZXNjcmlwdGlvbg==")
+                        .title("File title 1")
+                        .description("This is the first file description")
+                        .creationDate(LocalDateTime.now())
+                        .build(),
+                FileDto.builder()
+                        .fileData("VGhpcyBpcyB0aGUgZmlsZSBkZXNjcmlwdGlvbg==")
+                        .title("File title 2")
+                        .description("This is the second file description")
+                        .creationDate(LocalDateTime.now().plusMinutes(10))
+                        .build()
+        );
+
+        Page<FileDto> fileDtoPage = new PageImpl<>(fileDtoList);
+
+        when(fileService.findAllFiles(page, size, sortBy)).thenReturn(fileDtoPage);
+
+        ResponseEntity<Page<FileDto>> response = fileController.getFiles(page, size, sortBy);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(fileDtoPage, response.getBody());
+        verify(fileService).findAllFiles(page, size, sortBy);
     }
 
     @Test
