1c41fedfd6b11a6c01c92df7b991b31f175ca251
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-14 19:50:14 +0300

    Add create script for files table

---
+++ src/main/resources/db/changelog/changeset/001_create_files_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE files
+(
+    id            SERIAL PRIMARY KEY,
+    file_data     OID         NOT NULL,
+    title         VARCHAR(32) NOT NULL,
+    description   VARCHAR(500),
+    creation_date TIMESTAMP   NOT NULL DEFAULT NOW()
+);
